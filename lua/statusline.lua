acadad9435c594d8d2edc2f2180791ce7dfb9974
Duncan Leo <hello@duncanleo.me>
2021-11-27 16:31:57 +0800

    chore: configure lualine.nvim for tabline

--- lua/statusline.lua
+++ lua/statusline.lua
@@ -2,20 +2,28 @@ local lualine = require 'lualine'
 
 lualine.setup {
   options = {
-    theme = 'gruvbox'
+    theme = 'gruvbox',
+    disabled_filetypes = { 'NvimTree' }
   },
   sections = {
     lualine_a = {'mode'},
-    lualine_b = { 'g:coc_status' },
-    lualine_c = {'b:coc_git_blame'},
-    lualine_x = {
+    lualine_b = {
       {
         'filename',
         path = 1,
-        shorting_target = 40
       }
     },
+    lualine_c = {},
+    lualine_x = {'diff'},
     lualine_y = {'encoding', 'filetype'},
     lualine_z = {'location'}
   },
+  tabline = {
+    lualine_a = {'g:coc_status'},
+    lualine_b = {'buffers'},
+    lualine_c = {'b:coc_git_blame'},
+    lualine_x = {},
+    lualine_y = {'diagnostics'},
+    lualine_z = {'tabs'}
+  }
 }
