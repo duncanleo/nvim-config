6ff506c4f3c5a48f3e6db7e5c052734253539715
Duncan Leo <hello@duncanleo.me>
2022-04-23 22:20:33 +0800

    chore(nvimtree): use replacement for removed `auto_close` feature

--- lua/nvim_tree.lua
+++ lua/nvim_tree.lua
@@ -15,7 +15,6 @@ require'nvim-tree'.setup {
     enable = true,
     auto_open = true,
   },
-  auto_close          = true,
   open_on_tab         = false,
   hijack_cursor       = false,
   update_cwd          = false,
@@ -65,3 +64,12 @@ vim.api.nvim_set_keymap(
   ':sb NvimTree<CR>',
   { noremap = true }
 )
+
+vim.api.nvim_create_autocmd("BufEnter", {
+  nested = true,
+  callback = function()
+    if #vim.api.nvim_list_wins() == 1 and vim.api.nvim_buf_get_name(0):match("NvimTree_") ~= nil then
+      vim.cmd "quit"
+    end
+  end
+})
